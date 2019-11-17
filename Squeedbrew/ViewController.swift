8021cf67938b2d7b0feb772c8433dd06631a41ac
Johan Sandström <johan.sandstrom@squeed.com>
2019-11-17 16:33:34 +0100

    Fresh project

---
+++ Squeedbrew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Squeedbrew
+//
+//  Created by Johan Sandström on 2019-11-17.
+//  Copyright © 2019 Squeed. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
